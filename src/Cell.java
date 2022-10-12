b86319533e9a7194a98c902f586c295945a93662
Benjamin Giese <benjaminagiese@gmail.com>
2022-10-12 14:08:05 -0600

    Added some methods to Cell

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,14 @@
 public class Cell {
+    private CellState cell_state;
+
+
+
+    public CellState getCell_state() {
+        return cell_state;
+    }
     
+    public void setState(CellState state)
+    {
+        cell_state=state;
+    }
 }
