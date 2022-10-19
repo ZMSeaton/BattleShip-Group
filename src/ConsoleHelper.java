d1839f3c937bb1e0a18954565a1c8474bc36e8c5
Elizabeth McShane <connors2ea@gmail.com>
2022-10-19 11:45:11 -0600

    added convertInputToNumber method in ConcoleHelper

--- src/ConsoleHelper.java
+++ src/ConsoleHelper.java
@@ -1,8 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 public class ConsoleHelper {
-    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
     public static String getInput(final String prompt) {
         String inStr = "";
         System.out.print(prompt);
@@ -12,5 +14,29 @@ public class ConsoleHelper {
             System.out.println("Error reading from user");
         }
         return inStr;
+    }
+
+    public static int convertInputToNumber(String prompt, int min, int max) {
+
+        while (true) {
+            String n = prompt;
+
+            try {
+                int num = Integer.parseInt(n);
+
+                if (num >= min && num <= max) {
+                    return num;
+                } else {
+                    System.out.println("Please enter a valid choice.");
+                    continue;
+                }
+
+            } catch (NumberFormatException ex) {
+                System.out.println("Please enter a valid choice.");
+
+                continue;
+            }
+
+        }
     }
 }
