ce5d2ea6b31a3a0478f5edbe2270a33b4a407ff0
Elizabeth McShane <connors2ea@gmail.com>
2022-10-19 14:35:25 -0600

    Added some methods to Game class.

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,71 @@
 public class Game {
     private IPlayer[] players = new IPlayer[2];
+
+     public Game() throws Exception {
+        handleStartUpOption();
+    }
+
+    private void handleStartUpOption() throws Exception {
+        // options are 1,2,3,or 4
+        // print start up screen
+        printStartScreen();
+        int n = ConsoleHelper.convertInputToNumber(ConsoleHelper.getInput("Please enter your choice (1-4): "), 1, 4);
+        // this should prompt the player for their choice until a valid choice is made.
+
+        switch (n) {
+
+            case 1:
+                System.out.println("Rule printing will be implemented later.");
+                break;
+
+            case 2:
+                System.out.println("Scoreboard will be implemented later.");
+                break;
+
+            case 3:
+                System.out.println("Let's play some Battleship!");
+                determineNumOfPlayers();
+                break;
+
+            case 4:
+
+                System.out.println("Thank you for playing Battleship!");
+                System.exit(0);
+        }
+
+    }
+
+    private void determineNumOfPlayers() throws Exception {
+
+        // ask for the number of players "Please enter the number of players (1 or 2): "
+        // the only valid inputs are 1 or 2. Enforce this.
+        // do we capture player name here and hand it off to the player object?
+
+        String prompt = ConsoleHelper.getInput("Please enter the number of players (1 or 2): ");
+
+        int numberOfPlayer = ConsoleHelper.convertInputToNumber(prompt, 1, 2);
+
+        if (numberOfPlayer == 1) {
+            // create 1 humanPlayer and ask about game difficulty
+            // Player player1 = new Player;
+            System.out.println("This will create a single player game");
+
+        } else {
+            // create 2 humanPlayers
+            System.out.println("This will create a two player game.");
+
+        }
+
+    }
+
+    private static void printStartScreen() {
+        System.out.println("Welcome to Battleship!");
+        System.out.println("Please choose from the following options:");
+        System.out.println("1. Show the rules");
+        System.out.println("2. Show the scoreboard");
+        System.out.println("3. Play Battleship");
+        System.out.println("4. Exit the game");
+
+    }
     
 }
