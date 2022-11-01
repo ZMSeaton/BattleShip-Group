a272bdeb4291a5bbaae8792b34ab6d6ba7523d02
Elizabeth McShane <connors2ea@gmail.com>
2022-11-01 13:34:44 -0600

    finished create player  methods

--- src/Game.java
+++ src/Game.java
@@ -1,35 +1,36 @@
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 public class Game {
     private IPlayer[] players = new IPlayer[2];
-    //should we have separate instance variables for player1 and player2?
+
     private int playerIndex;
     private boolean isGameOver = false;
 
     public void playGame() throws Exception {
 
-        for(IPlayer player : players){
-        
-        while(isGameOver == false){
-            
-            //take shot
+        for (IPlayer player : players) {
 
-            
-            //give player feedback
+            while (isGameOver == false) {
 
-            //switch players. 
-            //anticheating display. return to begining of game loop.
+                // take shot
 
+                // give player feedback
+
+                // switch players.
+                // anticheating display. return to begining of game loop.
+
+            }
         }
     }
-}
-    
-    public static void handleStartUpOption() throws Exception {
 
-        while(true){
+    public void handleStartUpOption() {
+
+        while (true) {
 
             printStartScreen();
-            int n = ConsoleHelper.convertInputToNumber("Please enter your choice (1-4): ", " Please choose between options 1 through 4.", 1, 4);
+            int n = ConsoleHelper.convertInputToNumber("Please enter your choice (1-4): ",
+                    " Please choose between options 1 through 4.", 1, 4);
             // this should prompt the player for their choice until a valid choice is made.
 
             switch (n) {
@@ -47,7 +48,7 @@ public class Game {
                 case 3:
                     System.out.println("Let's play some Battleship!");
                     System.out.println("+----+----+----+----+----+----+----+");
-                    determineNumOfPlayers();
+                    createPlayers();
                     break;
 
                 case 4:
@@ -58,42 +59,47 @@ public class Game {
             }
 
         }
-    }   
+    }
 
-    private static void determineNumOfPlayers() throws Exception {
+    private void createPlayers(){
+        int n = determineNumOfPlayers();
+        if(n==2){
+            
+           players[0]= createHumanPlayer();
+           players[1]= createHumanPlayer();
+            
 
-        int numberOfPlayer = ConsoleHelper.convertInputToNumber("Please enter the number of players (1 or 2): ", "Please choose 1 or 2 players.", 1, 2);
-
-        if (numberOfPlayer == 1) {
-            // create 1 humanPlayer and ask about game difficulty for AI
-            String name = ConsoleHelper.getInput("Please enter the name of Player 1: ");
-            HumanPlayer player1 = new HumanPlayer(name);
-            AIPlayer player2 = new AIPlayer;
-            players[0] = player1;
-            players[1] = player2;
-
-
-
-            //System.out.println("This will create a single player game");
-            //Demo.gameDemo();
-
-        } else {
-            // create 2 humanPlayers
-            String name1 = ConsoleHelper.getInput("Please enter the name of Player 1: ");
-            HumanPlayer player1 = new HumanPlayer(name1);
-            String name2 = ConsoleHelper.getInput("Please enter the name of Player 2: ");
-            HumanPlayer player2 = new HumanPlayer(name2);
-            players[0] = player1;
-            players[1] = player2;
-
-            //System.out.println("This will create a two player game.");
-            //Demo.gameDemo();
+        }else{
+          players[0]= createHumanPlayer();
+          players[1]= createAIPlayers();
 
         }
+       
+    }
+
+    private int determineNumOfPlayers() {
+
+        int numberOfPlayer = ConsoleHelper.convertInputToNumber("Please enter the number of players (1 or 2): ",
+                "Please choose 1 or 2 players.", 1, 2);
+        return numberOfPlayer;
 
     }
 
-    private static void printStartScreen() {
+    private IPlayer createHumanPlayer() {
+        String name = ConsoleHelper.getInput("Please enter the name of Player 1: ");
+        HumanPlayer player = new HumanPlayer(name);
+        return player;
+
+    }
+
+    private IPlayer createAIPlayers() {
+        // ask for difficulty when we have more than 1 AI to play with
+        AIPlayer player = new AIPlayer();
+        return player;
+
+    }
+
+    private void printStartScreen() {
         System.out.println("Welcome to Battleship!");
         System.out.println("Please choose from the following options:");
         System.out.println("1. Show the rules");
@@ -103,9 +109,12 @@ public class Game {
 
     }
 
-    private void turnFeedbackTakeShot(Shot shot, ShotResult shotResult, Ship sunkShip){ //only a human player would use this method //hand method null if ship wasn't sunk
+    private void turnFeedbackTakeShot(Shot shot, ShotResult shotResult, Ship sunkShip) { // only a human player would
+                                                                                         // use this method //hand
+                                                                                         // method null if ship wasn't
+                                                                                         // sunk
         String result;
-        switch(shotResult){
+        switch (shotResult) {
             case MISS:
                 result = "missed.";
                 break;
@@ -121,9 +130,10 @@ public class Game {
         System.out.println("You're shot at " + shot.getHumanReadable() + " " + result);
     }
 
-    private void turnFeedbackReceiveShot(Shot lastShot, ShotResult lastShotResult, HumanPlayer currentPlayer, IPlayer opposingPlayer){ //only a human player would use this method
+    private void turnFeedbackReceiveShot(Shot lastShot, ShotResult lastShotResult, HumanPlayer currentPlayer,
+            IPlayer opposingPlayer) { // only a human player would use this method
         String result;
-        switch(lastShotResult){
+        switch (lastShotResult) {
             case MISS:
                 result = "missed your ships.";
                 break;
@@ -139,10 +149,11 @@ public class Game {
         System.out.println(opposingPlayer.getName() + "'s shot at " + lastShot.getHumanReadable() + " " + result);
     }
 
-    private void antiCheatScreen(HumanPlayer currentPlayer){ //only a human player would use this method
+    private void antiCheatScreen(HumanPlayer currentPlayer) { // only a human player would use this method
         try {
-            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor(); //should send clear screen command to terminal
-        } catch (Exception exception){
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor(); // should send clear screen command to
+                                                                                  // terminal
+        } catch (Exception exception) {
             System.out.println("Unsuccesful clearing of screen");
         }
         ConsoleHelper.getInput("Press enter to play...");
---
+++ test/GameClassTests.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class GameClassTests {
+    
+    @Test
+    public void testCreatePlayers(){
+
+    
+    }
+}
