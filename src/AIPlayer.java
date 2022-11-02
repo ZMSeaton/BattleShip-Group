4b042561a0fea7233c2d7004d3b4af267c5f2936
Benjamin Giese <benjaminagiese@gmail.com>
2022-11-02 13:52:17 -0600

    Starting to actually make the AIPlayer

--- src/AIPlayer.java
+++ src/AIPlayer.java
@@ -1,6 +1,40 @@
-/*public class AIPlayer implements IPlayer {
+import java.util.Random;
+public class AIPlayer implements IPlayer {
+Random random=new Random();
+String name;
 
-/// String name;
+@Override
+public String getName() {
+    // TODO Auto-generated method stub
+    return null;
+}
+
+@Override
+public Shot takeShot() {
+     Shot shot=new Shot(random.nextInt(9),random.nextInt(9));
+     return shot;
+}
+
+@Override
+public boolean allShipsAreSunk() {
+    // TODO Auto-generated method stub
+    return false;
+}
+
+@Override
+public ShotResult recieveShot(Shot shot) {
+    // TODO Auto-generated method stub
+    return null;
+}
+
+@Override
+public void recieveShotResult(ShotResult shotResult, Shot shot) {
+    // TODO Auto-generated method stub
+    
+}
+
+
+
 
 /// public String get_name() {
 //// return name;
@@ -13,5 +47,5 @@
 /// public boolean allShipsAreSunk() {
 
     }
-}
-*/ //Temporarily commented out to avoid java complaints
+///}
+///*/ //Temporarily commented out to avoid java complaints
