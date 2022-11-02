e05178479eb1151171562318ad11d411e86aab12
Benjamin Giese <benjaminagiese@gmail.com>
2022-11-02 14:02:15 -0600

    Revising AIPlayer

--- src/AIPlayer.java
+++ src/AIPlayer.java
@@ -1,35 +1,49 @@
 import java.util.Random;
 public class AIPlayer implements IPlayer {
 Random random=new Random();
-String name;
+protected String name;
+protected ShipBuilder shipBuilder = new ShipBuilder();
+protected OceanGrid oceanGrid;
+protected TargetGrid targetGrid;
 
+public AIPlayer(String name) {
+    this.name = name;
+    oceanGrid = new OceanGrid(shipBuilder.getShips());
+    targetGrid = new TargetGrid();
+}
 @Override
 public String getName() {
-    // TODO Auto-generated method stub
-    return null;
+   return name;
 }
 
 @Override
 public Shot takeShot() {
-     Shot shot=new Shot(random.nextInt(9),random.nextInt(9));
-     return shot;
+     Shot shot=new Shot(random.nextInt(10),random.nextInt(10));
+     while (true){
+     if (targetGrid.isShotValid(shot.getRow(), shot.getColumn())) {
+        return shot;
+    } else {
+      
+        continue;
+    }}
 }
 
 @Override
 public boolean allShipsAreSunk() {
-    // TODO Auto-generated method stub
+    if (oceanGrid.isAllShipsSunk()) {
+        return true;
+    }
     return false;
 }
 
 @Override
-public ShotResult recieveShot(Shot shot) {
-    // TODO Auto-generated method stub
-    return null;
+public ShotResult recieveShot(Shot gotshot) {
+    return oceanGrid.getShotResult(gotshot);
 }
 
 @Override
 public void recieveShotResult(ShotResult shotResult, Shot shot) {
-    // TODO Auto-generated method stub
+    targetGrid.updateTargetGrid(shot, shotResult);
     
 }
 
